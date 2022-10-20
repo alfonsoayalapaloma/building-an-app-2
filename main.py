00b4736344f57a77934dce4f54d15554dc0ad9c3
Alfonso Ayala <alfonsoayalapaloma@gmail.com>
2022-10-20 05:49:43 +0000

    student entity

--- main.py
+++ main.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 
 import datetime
+import random
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # [START gae_python38_datastore_store_and_fetch_times]
 # [START gae_python3_datastore_store_and_fetch_times]
@@ -48,6 +49,39 @@ def fetch_times(limit):
 # [END gae_python3_datastore_store_and_fetch_times]
 # [END gae_python38_datastore_store_and_fetch_times]
 
+def store_student( document, first_name, last_name, major, birthyear):
+    entity= datastore.Entity(key=datastore_client.key('student'))
+    entity.update({
+        'document': document,
+        'first_name': first_name,
+        'last_name': last_name,
+        'major': major,
+        'birthyear': birthyear
+    })
+    datastore_client.put(entity)
+
+def fetch_students(limit):
+    query= datastore_client.query(kind='student')
+    students=query.fetch(limit=limit)
+    print(students)
+    return students
+
+
+@app.route('/insert',methods = ['POST', 'GET'] )
+def insert():
+    if request.method == 'POST':
+        result = request.form
+        json_result = dict(result)
+        print(json_result)
+        store_student( json_result['document'],
+                       json_result['first_name'],
+                       json_result['last_name'],
+                       'Computer Science',
+                       json_result['birthyear'])
+        students=fetch_students(12)
+        return render_template("index.html", result=result, students=students)
+    return render_template('insert.html')
+
 
 # [START gae_python38_datastore_render_times]
 # [START gae_python3_datastore_render_times]
@@ -56,11 +90,18 @@ def root():
     # Store the current access time in Datastore.
     store_time(datetime.datetime.now(tz=datetime.timezone.utc))
 
-    # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
+    # Fetch the most recent 1 access times from Datastore.
+    times = fetch_times(1)
+
+    first_names=['Isaac','Albert','Robert','Enrico','Fiodor','Emanuel','Carl']
+    last_names=['Newton','Einstein','Hook','Tesla','Mendeliev','Kant','Sagan']
+
+
+    store_student(random.randint(0,101000000),random.choice(first_names),random.choice(last_names),'Physics',random.randint(0,2000))
+    students=fetch_students(12)
 
     return render_template(
-        'index.html', times=times)
+        'index.html', times=times, students=students)
 # [END gae_python3_datastore_render_times]
 # [END gae_python38_datastore_render_times]
 
